df2e91bc457a5088a3b4d2926d3321bd969acb81
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-29 12:22:12 -0500

    Write function to get next possible moves

--- main.js
+++ main.js
@@ -1,10 +1,39 @@
-function knightMoves(x, y) {
 
+
+function knightMoves(arr1, arr2) {
+    
 }
 
-const nextMoves = [];
-
-const possibility = for (let i = -2; i < 3; i++) {
-    if (i === 0) continue;
-    nextMoves.push
+function getMoves(x, y) {
+    const nextMoves = [];
+    for (let i = -2; i < 3; i++) {
+        if (i === 0) continue;
+        const possX = x + i;
+        if (possX < 0 || possX > 7) continue;
+        if (i === 1 || i === -1) {
+            const possY1 = y + 2;
+            if (possY1 > 0 && possY1 < 8) {
+                nextMoves.push([possX, possY1]);
+            }
+            const possY2 = y - 2;
+            if (possY2 > 0 && possY2 < 8) {
+                nextMoves.push([possX, possY2]);
+            }
+        } else if (i === 2 || i === -2) {
+            const possY1 = y + 1;
+            if (possY1 > 0 && possY1 < 8) {
+                nextMoves.push([possX, possY1]);
+            }
+            const possY2 = y - 1;
+            if (possY2 > 0 && possY2 < 8) {
+                nextMoves.push([possX, possY2]);
+            }
+        }
+    }
+    return nextMoves;
 }
+
+console.log(getMoves(7, 7));
+
+
+
