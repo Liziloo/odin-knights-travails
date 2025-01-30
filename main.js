45005305c8c1e8729cc1eb291dcb512c6aa53a0b
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-30 17:00:27 -0500

    Separate KnightMoves from GetPaths

--- main.js
+++ main.js
@@ -1,30 +1,67 @@
-
-
-function knightMoves(arr1, arr2) {
-    
+function knightMoves(coord1, coord2) {
+    const shortPaths = getPaths(coord1, coord2);
+    console.log(`=> You made it in ${shortPaths[0].length} moves! Here's your path:`);
+    shortPaths[0].forEach(element => {
+        console.log(element);
+    });
 }
 
-function getMoves(x, y) {
+function getPaths(coord, end, path = [], paths = []) {
+    if (arrayIncludesArray(path, coord)) {
+        return null;
+    };
+    path.push(coord);
+    if (JSON.stringify(coord) == JSON.stringify(end)) {
+        if (paths.length === 0 || paths[0].length === path.length) {
+            paths.push(path);
+        } else if (path.length < paths[0].length) {
+            paths = [];
+            paths.push(path);
+        }
+        return paths;
+    }
+    
+    const nextMoves = getMoves(coord);
+    let newShorts = [];
+    for (let i = 0; i < nextMoves.length; i++) {
+        const newPaths = getPaths(nextMoves[i], end, path, paths);
+        if (!newPaths || newPaths.length < 1) continue;
+        if (newShorts.length < 1 || newPaths[0].length < newShorts[0].length) {
+            newShorts = newPaths;
+        } else if (newShorts[0].length === newPaths[0].length) {
+            newShorts = newShorts.concat(newPaths);
+        } else {
+
+        }
+    }
+    return newShorts;
+}
+
+function arrayIncludesArray(arr1, arr2) {
+    return arr1.some(item => Array.isArray(item) && item.length === arr2.length && item.every((value, index) => value === arr2[index]));
+  }
+
+function getMoves(arr) {
     const nextMoves = [];
     for (let i = -2; i < 3; i++) {
         if (i === 0) continue;
-        const possX = x + i;
+        const possX = arr[0] + i;
         if (possX < 0 || possX > 7) continue;
         if (i === 1 || i === -1) {
-            const possY1 = y + 2;
+            const possY1 = arr[1] + 2;
             if (possY1 > 0 && possY1 < 8) {
                 nextMoves.push([possX, possY1]);
             }
-            const possY2 = y - 2;
+            const possY2 = arr[1] - 2;
             if (possY2 > 0 && possY2 < 8) {
                 nextMoves.push([possX, possY2]);
             }
-        } else if (i === 2 || i === -2) {
-            const possY1 = y + 1;
+        } else {
+            const possY1 = arr[1] + 1;
             if (possY1 > 0 && possY1 < 8) {
                 nextMoves.push([possX, possY1]);
             }
-            const possY2 = y - 1;
+            const possY2 = arr[1] - 1;
             if (possY2 > 0 && possY2 < 8) {
                 nextMoves.push([possX, possY2]);
             }
@@ -33,7 +70,4 @@ function getMoves(x, y) {
     return nextMoves;
 }
 
-console.log(getMoves(7, 7));
-
-
-
+knightMoves([3,3],[4,3])
